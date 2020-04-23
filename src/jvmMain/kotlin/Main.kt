a07447501359d4e3f81304a4492834a15dcc825b
ValeriiaFilimonova <valer.af@yandex.ru>
2020-04-23 20:44:11 +0300

    Added lantera package for simple timer console display

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,69 @@
 import alerts.AlertGenerators
 import alerts.sound.Sound.MORNING_DEW_LONG
-import alerts.sound.Sound.TING_A_LING
+import alerts.voice.JvmSpeechSynthesizer
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
+import ui.lantera.TimerTerminalFontConfiguration
+import kotlin.math.abs
+import kotlin.time.ExperimentalTime
+import kotlin.time.milliseconds
 
+@ExperimentalTime
 fun main() {
+    val fontConfiguration = TimerTerminalFontConfiguration(30)
+    val terminalFactory = DefaultTerminalFactory().apply {
+        setTerminalEmulatorTitle("Task Timer")
+        setTerminalEmulatorFontConfiguration(fontConfiguration)
+        setInitialTerminalSize(TerminalSize(29, 9))
+    }
+    val terminal: Terminal = terminalFactory.createTerminal()
+    val screen: Screen = TerminalScreen(terminal).apply { cursorPosition = null }
+
+    screen.startScreen()
+
     val properties = TimerPropertiesBuilder(15.SECONDS)
-        .tickInterval(500.MILLISECONDS)
-        .afterPassed(0.SECONDS, AlertGenerators.getVoiceAlertGenerator("timer started"))
-        .beforeLeft(2.SECONDS, AlertGenerators.getSoundAlertGenerator(TING_A_LING))
-        .repeatEvery(2.SECONDS, 2.SECONDS, 10.SECONDS, AlertGenerators.getSayTimeAlertGenerator())
+        .tickInterval(1.SECONDS)
+        .repeatEvery(5.SECONDS, generator =  AlertGenerators.getSayTimeAlertGenerator())
         .remindAfterFinishEvery(5.SECONDS, AlertGenerators.getVoiceAlertGenerator("stop the timer"))
         .finalAlarm(MORNING_DEW_LONG)
         .build()
 
     val timer = Timer(properties)
         .also {
-            it.onStart = { println("${System.currentTimeMillis()}: started") }
-            it.onStop = { println("${System.currentTimeMillis()}: stopped") }
-            it.onTick = { println("${it.elapsedTime / 1.SECONDS.toDouble() }sec") }
+            it.onStart = {
+                display(screen, it.duration.toLong())
+            }
+            it.onTick = {
+                display(screen, (it.duration.toLong() - it.elapsedTime.toLong()))
+            }
         }
 
     timer.start()
-    Thread.sleep(1.MINUTES.toLong())
+    Thread.sleep(12.MINUTES.toLong())
     timer.stop()
+
+    (DependenciesFactory.getSpeaker() as JvmSpeechSynthesizer).close()
 }
+
+@ExperimentalTime
+private fun display(screen: Screen, timeLeft: Long) {
+    abs(timeLeft).milliseconds.toComponents { days, hours, minutes, seconds, _ ->
+        var text = "${days.pad()}:${hours.pad()}:${minutes.pad()}:${seconds.pad()}"
+
+        if (timeLeft < 0) {
+            text = "-$text"
+        }
+
+        val size = screen.terminalSize
+        val column = (size.columns - text.length) / 2
+        val row = size.rows / 2
+
+        screen.newTextGraphics().putString(column, row, text)
+        screen.refresh()
+    }
+}
+
+private fun Int.pad(): String = toString().padStart(2, '0')
---
+++ src/jvmMain/kotlin/ui/lantera/TimerTerminalFontConfiguration.kt
@@ -0,0 +1,9 @@
+package ui.lantera
+
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration
+import java.awt.Font
+
+class TimerTerminalFontConfiguration(fontSize: Int) : SwingTerminalFontConfiguration(
+    true, BoldMode.EVERYTHING_BUT_SYMBOLS,
+    *(filterMonospaced(*selectDefaultFont()).map { f -> Font(f.name, f.style, fontSize) }.toTypedArray())
+)
